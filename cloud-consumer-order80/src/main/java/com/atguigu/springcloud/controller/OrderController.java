8c7fc5bc0a192ff3759aa9299bfb50fb22b89685
w <1358208941@qq.com>
2020-05-22 21:36:11 +0800

    first add

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,54 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.lb.LoadBalancer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping(value = "/consumer")
+public class OrderController {
+
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private LoadBalancer loadBalancer;
+
+    @GetMapping("/payment/create")
+    public CommonResult<Payment> create(Payment payment) {
+        System.out.println("hot");
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);  //写操作
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if (instances == null || instances.size() <= 0) {
+            return null;
+        }
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
+    }
+}
---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/lb/MyLB.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.lb;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class MyLB implements LoadBalancer {
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    private final int getNext() {
+        int current;
+        int next;
+        do {
+            current = atomicInteger.get();
+            next = current >= Integer.MAX_VALUE ? 0 : current + 1;
+        } while (!atomicInteger.compareAndSet(current, next));
+        return next;
+    }
+
+    @Override
+    public ServiceInstance instances(List<ServiceInstance> serviceInstances) {
+        int index = getNext() % serviceInstances.size();
+        return serviceInstances.get(index);
+    }
+}
